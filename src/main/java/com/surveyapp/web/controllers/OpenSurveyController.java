6d7542bde6c0611eee789d6218183e4ac2491b2b
Lavanya Kandukuri <lavanya.k9110@gmail.com>
2018-05-07 23:56:08 -0700

    added token generation files

--- src/main/java/com/surveyapp/web/controllers/OpenSurveyController.java
+++ src/main/java/com/surveyapp/web/controllers/OpenSurveyController.java
@@ -14,9 +14,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 public class OpenSurveyController {
@@ -32,11 +34,40 @@ public class OpenSurveyController {
 
     public static final String SHOW_SURVEY = "surveyee/showSurvey";
 
-    @RequestMapping(value="/survey/{sId}", method = RequestMethod.GET)
-    public String getSurvey(@PathVariable int sId, Model m){
+//    @RequestMapping(value="/survey/{sId}", method = RequestMethod.GET)
+//    public String getSurvey(@PathVariable int sId, Model m){
+//
+//        //To populate HTML, we need survey name,
+//        Survey survey =  surveyService.getSurvey(sId);
+//        List<Question> questions = questionService.getQuestions(survey);
+//        m.addAttribute("survey", survey);
+//        m.addAttribute("questions",questions);
+//        return SHOW_SURVEY;
+//
+//
+////        //To populate HTML, we need survey name,
+////        Survey survey =  surveyService.getSurvey(sId);
+////        m.addAttribute("survey", survey);
+////        List<Question> questions = questionService.getQuestions(survey);
+////        for (Question question : questions) {
+////            List<QuestionOption> options = optionService.getOptions(question);
+////            m.addAttribute("question",question);
+////            m.addAttribute("options",options);
+////        }
+//    }
+
+    @RequestMapping(value="/takesurvey", method = RequestMethod.GET)
+    public String getSurvey(@RequestParam(value="surveyId") int surveyId,
+                        @RequestParam(value = "token" ) String token,
+                            Locale locale,
+                            ModelMap m){
 
         //To populate HTML, we need survey name,
-        Survey survey =  surveyService.getSurvey(sId);
+
+
+        Survey survey =  surveyService.getSurvey(surveyId);
+
+        System.out.println(survey.getSurveyType());
         List<Question> questions = questionService.getQuestions(survey);
         m.addAttribute("survey", survey);
         m.addAttribute("questions",questions);
