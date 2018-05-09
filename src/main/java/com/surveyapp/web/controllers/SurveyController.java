14e24cacad7b0970e94bb9870bd8e77b6122e449
Ambika Bohra <bohraambika@yahoo.com>
2018-05-09 03:10:40 -0700

    comments

--- src/main/java/com/surveyapp/web/controllers/SurveyController.java
+++ src/main/java/com/surveyapp/web/controllers/SurveyController.java
@@ -7,38 +7,119 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
-@RestController
+@Controller
 public class SurveyController{
+
+
+    public static final String SURVEYOR_HOME = "surveyor/createSurvey";
 
     @Autowired
     private SurveyService surveyService;
+
+    @Autowired
+    private UserService userService;
     
 
-    @RequestMapping(value="/survey/{sId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getSurveyDetail(@PathVariable(value = "sId") int sId, Model model) {
+//    @RequestMapping(value="/survey/{sId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<?> getSurveyDetail(@PathVariable(value = "sId") int sId, Model model) {
+//
+//       Survey survey =  surveyService.getSurvey(sId);
+//       model.addAttribute("survey", survey);
+//       return new ResponseEntity<>(survey, HttpStatus.OK);
+//    }
+//
+//
+//
+//    @RequestMapping(value="/survey/{email}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<?> surveyForm(@PathVariable String email,
+//                                        @RequestParam(value= "description") String description,
+//                                        @RequestParam(value = "sType") String surveyType,
+//                                        @RequestParam(value = "endTime", required = false) String endTime){
+//
+//        Survey surveyObj = surveyService.createSurvey(email, description, surveyType, endTime);
+//        return new ResponseEntity<>(surveyObj, HttpStatus.OK);
+//
+//    }
 
-       Survey survey =  surveyService.getSurvey(sId);
-       model.addAttribute("survey", survey);
-       return new ResponseEntity<>(survey, HttpStatus.OK);
+//    @RequestMapping(value="/survey", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+//    public String saveSurvey( Model model) {
+//
+//      //  Survey survey =  surveyService.getSurvey(sId);
+//        model.addAttribute("survey", new Survey());
+//        return "/survey";
+//    }
+
+//    @RequestMapping(value="/survey", method=RequestMethod.POST)
+//    public String contactSubmit(@ModelAttribute Survey survey, BindingResult bindingResult, Model model) {
+//        if (bindingResult.hasErrors()) {
+//            //errors processing
+//        }
+//        model.addAttribute("survey", survey);
+//        return "redirect:/";
+//    }
+
+
+//    @PostMapping("/savequestion")
+//    public String signUpForm(ModelMap model) {
+//   /* System.out.print("@@@@@@@@@@@@"+title);*/
+////        Survey survey = surveyService.getSurveyByName(title);
+////
+////        if(survey != null){
+////            model.addAttribute("survey", survey);
+////        }
+////        else
+//        Survey survey = new Survey();
+////        model.addAttribute("survey", survey);
+//        System.out.println("@@@@@@@@@@@@@@@@@@@@@"+model.get("quesPlaceHolder"));
+////        System.out.println(survey.getDescription());
+//        return "index";
+//    }
+//
+//    @PostMapping(value = "/survey")
+//    public String signUpSubmit(@ModelAttribute("survey") Survey survey,
+//                               BindingResult bindingResult,
+//                               ModelMap modelMap, RedirectAttributes redirectAttributes)
+//    {
+//        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//        String userName = auth.getName(); //get logged in username
+//        System.out.println(modelMap.size());
+//
+//        String email = userService.findByUserName(userName).getEmail();
+//
+//        Survey obj = surveyService.createSurvey(email, "survey11","general", survey.getEndTime());
+//
+//        return "redirect:/savesurvey";
+//    }
+
+
+
+    //Sign up form
+    @GetMapping("/survey")
+    public String signUpForm(Model model) {
+        model.addAttribute("survey", new Survey());
+        return "survey";
     }
 
-
-
-    @RequestMapping(value="/survey/{email}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> surveyForm(@PathVariable String email,
-                                        @RequestParam(value= "description") String description,
-                                        @RequestParam(value = "sType") String surveyType,
-                                        @RequestParam(value = "endTime", required = false) String endTime){
-
-        Survey surveyObj = surveyService.createSurvey(email, description, surveyType, endTime);
-        return new ResponseEntity<>(surveyObj, HttpStatus.OK);
-
+    @PostMapping(value = "/survey")
+    public String signUpSubmit(@ModelAttribute("survey") Survey survey,
+                               BindingResult bindingResult,
+                               ModelMap modelMap)
+    {
+        System.out.println(survey.getDescription() + survey.getQuestions().size());
+//        Survey obj = surveyService.createSurvey(email, description, surveyType, endTime);
+        return "verification";
     }
+
 
 }
 
--- src/main/java/com/surveyapp/web/controllers/SurveyorController.java
+++ src/main/java/com/surveyapp/web/controllers/SurveyorController.java
@@ -1,14 +1,37 @@
 package com.surveyapp.web.controllers;
 
+import com.surveyapp.backend.persistence.domain.backend.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.surveyapp.backend.persistence.domain.*;
+import com.surveyapp.backend.service.*;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 @Controller
 public class SurveyorController {
 
+
     public static final String SURVEYOR_HOME = "surveyor/surveyorHome";
+    public static final String SURVEYOR_RESPONSE = "surveyor/surveyResponse";
+
+    public static final String SURVEYOR_STATS = "surveyor/stats";
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SurveyService surveyService;
+
 
     @RequestMapping(value = "/surveyorhome", method = RequestMethod.GET)
     public String getSurveryorHome(ModelMap model){
@@ -40,7 +63,15 @@ public class SurveyorController {
     @RequestMapping(value = "/surveyresponse", method = RequestMethod.GET)
     public String surveyResponseGet(ModelMap model){
 
-        model.addAttribute("role","surveyor");
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userName = auth.getName(); //get logged in username
+        List<String> surveys = getSurveyDescription(userName);
+//
+//        model.addAttribute("surveys", surveys);
+//        return SURVEYOR_HOME;
+
+        model.addAttribute("surveys","surveys");
         return CHECK_RESPONSES;
     }
 
@@ -52,6 +83,92 @@ public class SurveyorController {
         model.addAttribute("role","surveyor");
         return EDIT_SURVEY;
     }
+
+
+    @RequestMapping(value="/surveyor/stats", method = RequestMethod.GET)
+    public String getSurveyDetails(@RequestParam("description") String surveyName, ModelMap model){
+        Survey survey = surveyService.getSurveyByName(surveyName);
+
+        System.out.println("@@@@@@@@@@@@@@"+survey.getSurveyType());
+
+        //get participant count
+        int count =0;
+        List<Token> tokens = survey.getTokens();
+        String type = survey.getSurveyType();
+        List<Question> questions = survey.getQuestions();
+
+        if(type.equals("general") || type.equals("gpen")) {
+
+            System.out.println("@@@@@@@@@@@@@@"+ tokens.get(0).getTokenId());
+            Token token = tokens.get(0);
+            count = token.getParticipants().size(); // count of participant
+
+        }
+        else if(type == "closed"){
+            count = tokens.size();
+        }
+
+        int submissionCount = questions.get(0).getResponses().size();
+        // get the size of the list of responses of one question
+        System.out.println("@@@@@@@@@@@@@@"+ count + " "+ submissionCount);
+        double participantRate = submissionCount* 1.0 / count;
+
+//        List<String> stats = new ArrayList<>();
+//        stats.add(survey.getDescription());
+//        stats.add(survey.getSurveyType());
+//        stats.add(String.valueOf(survey.getSurveyId())); //add surveyId
+//        stats.add(String.valueOf(survey.getCurrent())); //add start date
+//        stats.add(survey.getEndTime());     //add end time
+//        stats.add( String.valueOf(count));  //add participant count
+//        stats.add(String.valueOf(submissionCount));
+//        stats.add( String.valueOf(participantRate)); // add participation rate
+//        HashMap<Question, List<QuestionOption>> hmap = new HashMap<>();
+        model.addAttribute("stats", survey);
+        model.addAttribute("counter",new Counter());  // for option counter
+        return SURVEYOR_STATS;
+    }
+
+    @RequestMapping(value="/stats", method = RequestMethod.GET)
+    public String getQuestionResponses(@RequestParam("surveyId") String sId, ModelMap model) {
+
+        Survey survey = surveyService.getSurvey(Integer.valueOf(sId));
+        List<Question> questions = survey.getQuestions();
+
+        model.addAttribute("questions", questions);
+
+        return SURVEYOR_STATS;
+
+    }
+
+
+    @RequestMapping(value = "/surveyorresponse", method = RequestMethod.GET)
+    public String getSurveyor( ModelMap model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userName = auth.getName(); //get logged in username
+        List<String> surveys = getSurveyDescription(userName);
+
+        model.addAttribute("surveys", surveys);
+        return SURVEYOR_RESPONSE;
+    }
+//
+    public List<String> getSurveyDescription(String userName){
+
+        List<Survey> surveyList = userService.findByUserName(userName).getSurveys();
+        List<String> surveys = new ArrayList<>();
+        for(Survey survey: surveyList)
+            surveys.add(survey.getDescription());
+        return surveys;
+    }
+
+//    @RequestMapping(value = "/stats", method = RequestMethod.GET)
+//    public String getStats( ModelMap model){
+//
+//        List<String> surveys = getSurveyDescription(userName);
+//        model.addAttribute("surveys", surveys);
+//        // model.addAttribute("role","surveyor");
+//        // model.addAttribute(PAYLOAD_MODEL_KEY_NAME, new ProAccountPayload());
+//        return SURVEYOR_STATS;
+//    }
 
 
 }
